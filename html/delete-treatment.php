30c1309b8540a633c95b9f89d0c1c5d492db48d3
Graim_lee <jeremy.sagnard@gmail.com>
2025-05-30 03:14:33 +0200

    🥕Chat finished, it is great.
    To use it you need the database sent through discord

--- html/delete-treatment.php
+++ html/delete-treatment.php
@@ -5,7 +5,7 @@ $user_id = $_SESSION['User_id'];
 
 $servername = "localhost";
 $username = "root";
-$password = "Antoine-972";
+$password = "YourPassword";
 $database = "Troc_carrot";
 
 $conn = new mysqli($servername, $username, $password, $database);
